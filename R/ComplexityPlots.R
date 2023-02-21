5af8b805fcc2cbc6265ff54940e593ad4753fc21
Mollie Asbury <masbury@hawaii.edu>
2023-02-21 13:15:11 -1000

    old but pushing

--- R/ComplexityPlots.R
+++ R/ComplexityPlots.R
@@ -2,73 +2,6 @@ library(ggplot2)
 library(latex2exp)
 library(stringr)
 
-########### Isolating Comp data & adding stuff
-
-BaseD <- "C:\\Users\\Mollie.Asbury\\GitHub\\MBIO740_AMD\\output\\"
-IntCp <- c("LowIntComp","IntIntComp")
-SimTy <- c("ComplexitySims", "DisturbanceSims")
-s <- 1
-
-dataf <- data.frame()
-for (c in 1:2){
-  Dir <-  paste0(BaseD, SimTy[s], "\\", IntCp[c], "\\")
-  ls  <- list.files(Dir)
-  for (l in 1:length(ls)){
-    load(paste0(Dir,ls[l]))
-    name <- str_split(ls[l], pattern = "_")[[1]]
-    
-    time  <- seq(100)
-    
-    #fpMod <- lm(FullModel$FishPopulation[3:20,1] ~ time[3:20])
-    #fhMod <- lm(FullModel$Harvest[3:20,1] ~ time[3:20])
-    #lMod  <- lm(FullModel$Lambdas[3:20] ~ time[3:20])
-    cMod  <- lm(FullModel$rawComplex[3:20] ~ time[3:20])
-    
-    #fishPopSlp <- coef(fpMod)[[2]]
-    #fishPopFin <- FullModel$FishPopulation[100,1]
-    #harvestSlp <- coef(fhMod)[[2]]
-    #harvestFin <- FullModel$Harvest[100,1]
-    #lambdaSlp  <- coef(lMod)[[2]]
-    #lambdaFin  <- FullModel$Lambdas[100]
-    complxSlp  <- coef(cMod)[[2]]
-    complxFin  <- FullModel$rawComplex[100]
-    compDevFin <- FullModel$dComplex[100]
-    compDevVar <- sd(FullModel$dComplex)
-    CompType   <- name[5]
-    compDevMax <- max(FullModel$dComplex)
-    
-    if(length(name)  == 7){
-      CompVers <- str_split(name[7], pattern = ".rdata")[[1]][1]
-    }
-    else{CompVers <- "V1"}
-    
-    
-    ds <- data.frame(#fishPopSlp=fishPopSlp,
-      #fishPopFin=fishPopFin,
-      #harvestSlp=harvestSlp,
-      #harvestFin=harvestFin,
-      #lambdaSlp=lambdaSlp,
-      #lambdaFin=lambdaFin,
-      complxSlp=complxSlp,
-      complxFin=complxFin,
-      compDevFin=compDevFin,
-      compDevVar=compDevVar,
-      compDevMax=compDevMax,
-      #cpStartV=IntCp[c],
-      CompType=CompType,
-      CompVers=CompVers,
-      RecVal=as.factor(name[3]),
-      FishingEffrt=as.factor(name[1]),
-      IntComp=IntCp[c])
-    
-    dataf <- rbind(dataf, ds)
-  }
-}
-
-ns <- paste0(SimTy[s], "_heatmapCompData_MA.rdata")
-save(dataf, file=paste0(BaseD,ns))
-
-
 ########### Functions
 CDFs <- function(x, mean, scale){
   CDF <- 1 / (exp(-(x-mean)/ scale) + 1)
@@ -94,13 +27,9 @@ lines(sort(10^size$x), sort(data3$x), type = 'l', lwd = 2, col = "darkorange")
 
 ########### Load data 
 
-BaseD <- "C:\\Users\\Mollie.Asbury\\GitHub\\MBIO740_AMD\\output\\"
-ns    <- "ComplexitySims_heatmapCompData_MA.rdata"
-load(file=paste0(BaseD,ns)) ### THIS IS ONLY V2 DATA
-
-V2datsL <- dataf[dataf$IntComp == "LowIntComp",]
-V2datsI <- dataf[dataf$IntComp == "IntIntComp",]
-Rvals  <- unique(V2datsL$RecVal)
+BaseD <- "~/Documents/GitHub/MegaModelCubed/"
+ns    <- "output/data/RecvsFish/Simulations_results_041122.rdata"
+load(file=paste0(BaseD,ns)) 
 
 CompFun <- c("Exp", "Logit", "Log")
 med.col <- c("orange","red","purple")
